95f4546139cc3304d10e2c0eab760dab3beb1125
louisanna <36931697+louisanna@users.noreply.github.com>
2026-07-24 18:52:20 +0800

    Delete files directory

--- files/overlay/AdGuardHome.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh /etc/rc.common
-# 清理旧目录，重建权限完整目录
-/overlay/AdGuardHome/AdGuardHome -s stop
-rm -rf /tmp/AdGuardHome
-mkdir -p -m 755 /tmp/AdGuardHome
-/overlay/AdGuardHome/AdGuardHome -s restart
