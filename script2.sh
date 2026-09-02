20032b98fa613060761a6eac1262356d37fb36b8
louisanna <36931697+louisanna@users.noreply.github.com>
2026-09-02 19:56:38 +0800

    Update script2.sh

--- script2.sh
+++ script2.sh
@@ -67,9 +67,3 @@ EOF
 make defconfig
 
 grep -E "bash|curl|ethtool|miniupnpd|irqbalance|block-mount|luci-app-upnp|luci-app-irqbalance|zh-cn" .config
-
-# 查找并只给以 #! 开头的文件加执行权限
-find files/etc/init.d files/etc/hotplug.d files/etc/ppp -type f 2>/dev/null | \
-  while read -r file; do
-    head -c 2 "$file" | grep -q '^#!' && chmod +x "$file"
-  done
