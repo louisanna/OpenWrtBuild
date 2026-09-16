0825fd13384c74cb8516e7c66af69bc7f3ceb279
louisanna <36931697+louisanna@users.noreply.github.com>
2026-09-16 20:46:03 +0800

    Update check_updates.sh

--- op/usr/raspberry/check_updates.sh
+++ op/usr/raspberry/check_updates.sh
@@ -4,7 +4,7 @@
 BARK_URL="https://bark.free.svipss.top/brjTD2MmUaTTVxRYh8TTZC/"
 
 # 更新软件源
-cat /rom/lib/apk/db/installed > /lib/apk/db/installed
+#cat /rom/lib/apk/db/installed > /lib/apk/db/installed
 apk update > /dev/null 2>&1
 
 # 执行模拟升级并捕获输出
